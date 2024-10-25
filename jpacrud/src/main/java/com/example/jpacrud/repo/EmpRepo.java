929e3a9db5dd82a8eacd874e363c693345c9f552
Amol <Amol@LAPTOP-FHLR08P2>
2024-10-25 12:09:35 +0530

    JPA CRUD

--- jpacrud/src/main/java/com/example/jpacrud/repo/EmpRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpacrud.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.jpacrud.entities.Emp;
-
-public interface EmpRepo extends JpaRepository<Emp, Long> {
-
-}
